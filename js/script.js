23a78d57258741ba60bec69df0128fc218cf16ca
Adrian <boniekadi@gmail.com>
2022-10-25 19:38:33 +0200

    added info and panel sections

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const navBtn = document.querySelector(".hamburger");
 const items = document.querySelector(".nav-mobile");
+const a = document.querySelectorAll(".links");
 
 const handleNav = () => {
 	navBtn.classList.toggle("is-active");
