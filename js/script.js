16058f78d48aac4f0a808ecf8815f7cfebf6f030
Adrian <boniekadi@gmail.com>
2022-10-31 13:11:15 +0100

    completing pages, adding a footer and improving mobile navigation

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const navBtn = document.querySelector(".hamburger");
 const items = document.querySelector(".nav-mobile");
 const a = document.querySelectorAll(".links");
+const copy = document.querySelector(".footer-year");
 
 const handleNav = () => {
 	navBtn.classList.toggle("is-active");
@@ -8,3 +9,13 @@ const handleNav = () => {
 };
 
 navBtn.addEventListener("click", handleNav);
+
+a.forEach((item) =>
+	item.addEventListener("click", () => items.classList.remove("active"))
+);
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear();
+	copy.innerHTML = year;
+};
+handleCurrentYear();
